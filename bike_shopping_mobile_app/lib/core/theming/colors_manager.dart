fbe1dadbb8a0027e4284f0540a4a2e891de1f6a5
Younes Benmoussa <devbenmoussayounes@gmail.com>
2025-05-03 20:36:06 +0100

    feature(theming) ✨ add theming colors manager

---
+++ bike_shopping_mobile_app/lib/core/theming/colors_manager.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ColorsManager {
+  static const Color primaryDarkGray = Color(0xFF242C3B);
+  static const Color blueGray = Color(0xFF353F54);
+  static const Color bluishGray = Color(0xFF222834);
+
+  static const Color skyBlue = Color(0xFF37B6E9);
+  static const Color blue = Color(0xFF37B6E9);
+}
