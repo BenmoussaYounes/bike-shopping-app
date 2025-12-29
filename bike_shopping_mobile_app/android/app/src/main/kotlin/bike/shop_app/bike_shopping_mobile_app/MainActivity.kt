7894fbbbab2446ea3509e72d5eb0d48e52768e3e
younes <devbenmoussayounes@gmail.com>
2025-12-29 14:00:04 +0100

    update repo to latest flutter version

--- bike_shopping_mobile_app/android/app/src/main/kotlin/bike/shop_app/bike_shopping_mobile_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package bike.shop_app.bike_shopping_mobile_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ bike_shopping_mobile_app/android/app/src/main/kotlin/com/bike_shopping/mobile/bike_shopping_mobile_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.bike_shopping.mobile.bike_shopping_mobile_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
